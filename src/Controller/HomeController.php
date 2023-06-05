8883726651b69f1ca9b7635e9da636467a956335
IssamLammri <issamlamri34000@gmail.com>
2023-06-05 20:40:21 +0200

    trainer front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\RoomsRepository;
+use App\Repository\TrainersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+
+    public function __construct(
+        private RoomsRepository $roomsRepository,
+        private TrainersRepository $trainersRepository
+    )
     {
+
+    }
+
+
+    #[Route('/', name: 'app_home')]
+    public function index(
+    ): Response
+    {
+        $rooms = $this->roomsRepository->findAll();
+        $trainers = $this->trainersRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'rooms' => $rooms,
+            'trainers' => $trainers,
         ]);
     }
 }
